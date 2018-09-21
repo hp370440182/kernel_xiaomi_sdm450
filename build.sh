3bbdb1ffa0dbe57539b02ba0f7657c35e0a10cdd
ChalapathiRevanth <32573514+ChalapathiRevanth@users.noreply.github.com>
2018-09-21 10:18:32 +0530

    Update Build.sh

--- build.sh
+++ build.sh
@@ -4,3 +4,7 @@ export CROSS_COMPILE=~/Toolchain/bin/aarch64-linux-android-
 mkdir output
 make -C $(pwd) O=output rosy_defconfig
 make -j32 -C $(pwd) O=output
+cp -r output/arch/arm64/boot/Image.gz-dtb zip/
+cd zip
+mv Image.gz-dtb zImage 
+zip -r Thunder-Kernel-Rosy-v1.zip *

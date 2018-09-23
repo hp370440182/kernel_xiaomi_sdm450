5e2f1d9654361e761764fbdc725ca7819a10080e
ChalapathiRevanth <princechalapathi16@gmail.com>
2018-09-23 07:37:48 +0000

    Revert "fork: boost the cpu to max freq when an app is launched"
    This reverts commit e3371873c91ee74887acdfeff496817fdcdea272.

--- kernel/fork.c
+++ kernel/fork.c
@@ -76,8 +76,7 @@
 #include <linux/aio.h>
 #include <linux/compiler.h>
 #include <linux/kcov.h>
-#include <linux/cpufreq.h>
-#include <linux/cpu_boost.h>
+
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
 #include <asm/uaccess.h>
@@ -1655,9 +1654,6 @@ long do_fork(unsigned long clone_flags,
 	struct task_struct *p;
 	int trace = 0;
 	long nr;
-
-	if (is_zygote_pid(current->pid))
-		do_input_boost_max();
 
 	/*
 	 * Determine whether and which event to report to ptracer.  When

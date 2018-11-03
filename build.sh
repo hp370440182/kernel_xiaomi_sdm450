03ec57d6fea13bbca6c93769695aff935bf5df6c
Revanth <princechalapathi16@gmail.com>
2018-11-03 12:59:04 +0000

    Birth Of Thunder-Storm-V1.1

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ make -j32 -C $(pwd) O=output
 cp -r output/arch/arm64/boot/Image.gz-dtb zip/
 cd zip
 mv Image.gz-dtb zImage 
-zip -r THUNDER-STORM-KERNEL-V2.zip *
+zip -r THUNDER-STORM-KERNEL-ROSY-V1.1.zip *
